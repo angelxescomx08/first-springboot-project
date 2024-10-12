76d36022d233ad0835e0eb20037df6b6476504a2
ahxdevelopmx <91965647+ahxdevelopmx@users.noreply.github.com>
2024-10-11 19:57:26 -0600

    request mapping

--- src/main/java/com/angel/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
+++ src/main/java/com/angel/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
@@ -2,13 +2,15 @@ package com.angel.curso.springboot.webapp.springboot_web.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Map;
 import java.util.HashMap;
 
 @RestController
+@RequestMapping("/api")
 public class UserRestController {
-  @GetMapping("/details-rest")
+  @GetMapping("/details")
   public Map<String,Object> getMethodName() {
     Map<String,Object> reponse = new HashMap<String,Object>();
 
