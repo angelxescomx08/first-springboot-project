da0e2550b64dd78b6f1b70104431e35ef8c533d1
ahxdevelopmx <91965647+ahxdevelopmx@users.noreply.github.com>
2024-10-14 20:05:36 -0600

    modelo usuario

--- src/main/java/com/angel/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
+++ src/main/java/com/angel/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
@@ -1,6 +1,9 @@
 package com.angel.curso.springboot.webapp.springboot_web.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import com.angel.curso.springboot.webapp.springboot_web.models.User;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -13,10 +16,10 @@ public class UserRestController {
   @GetMapping("/details")
   public Map<String,Object> getMethodName() {
     Map<String,Object> reponse = new HashMap<String,Object>();
+    User user = new User("Ángel", "Hernández");
 
     reponse.put("title", "Mi título");
-    reponse.put("name", "Ángel");
-    reponse.put("lastname", "Hernández");
+    reponse.put("user", user);
 
     return reponse;
   }
